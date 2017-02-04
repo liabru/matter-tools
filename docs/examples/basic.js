6df5381ada02bdbffa06cc6c2b28d2d6c9e2142e
liabru <liabru@brm.io>
2017-02-04 18:20:16 +0000

    simplify example

--- docs/examples/basic.js
+++ docs/examples/basic.js
@@ -1,31 +1,28 @@
-function MatterBasic() {
-  // install plugins
-  Matter.use(
-    'matter-wrap'
-  );
+var Example = Example || {};
 
-  // module aliases
+Example.basic = function() {
   var Engine = Matter.Engine,
-      Runner = Matter.Runner,
-      Render = Matter.Render,
-      World = Matter.World,
-      Body = Matter.Body,
-      Mouse = Matter.Mouse,
-      Common = Matter.Common,
-      Bodies = Matter.Bodies;
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
 
   // create engine
-  var engine = Engine.create();
+  var engine = Engine.create(),
+    world = engine.world;
 
   // create renderer
   var render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-      width: Math.min(document.body.clientWidth, 1024),
-      height: Math.min(document.body.clientHeight, 1024),
-      showAngleIndicator: true,
-      wireframeBackground: '#0f0f13'
+      width: Math.min(document.documentElement.clientWidth, 800),
+      height: Math.min(document.documentElement.clientHeight, 600),
+      showAngleIndicator: true
     }
   });
 
@@ -35,47 +32,65 @@ function MatterBasic() {
   var runner = Runner.create();
   Runner.run(runner, engine);
 
-  // create demo scene
-  var world = engine.world;
-  world.gravity.scale = 0;
+  // add bodies
+  var stack = Composites.stack(20, 20, 10, 5, 0, 0, function(x, y) {
+    var sides = Math.round(Common.random(1, 8));
 
-  // add some random bodies
-  for (var i = 0; i < 150; i += 1) {
-    var body = Bodies.polygon(
-      Common.random(0, render.options.width), 
-      Common.random(0, render.options.height),
-      Common.random(1, 5),
-      Common.random() > 0.9 ? Common.random(15, 25) : Common.random(5, 10),
-      {
-        friction: 0,
-        frictionAir: 0,
-        wrap: {
-          min: {
-            x: 0,
-            y: 0
-          },
-          max: {
-            x: render.canvas.width,
-            y: render.canvas.height
-          }
-        }
+    // triangles can be a little unstable, so avoid until fixed
+    sides = (sides === 3) ? 4 : sides;
+
+    // round the edges of some bodies
+    var chamfer = null;
+    if (sides > 2 && Common.random() > 0.7) {
+      chamfer = {
+        radius: 10
+      };
+    }
+
+    switch (Math.round(Common.random(0, 1))) {
+    case 0:
+      if (Common.random() < 0.8) {
+        return Bodies.rectangle(x, y, Common.random(25, 50), Common.random(25, 50), { chamfer: chamfer });
+      } else {
+        return Bodies.rectangle(x, y, Common.random(80, 120), Common.random(25, 30), { chamfer: chamfer });
       }
-    );
-
-    Body.setVelocity(body, {
-      x: Common.random(-3, 3) + 3, 
-      y: Common.random(-3, 3) + 3
-    });
-
-    World.add(world, body);
-  }
-
-  // add mouse control
-  var mouseConstraint = Matter.MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
+    case 1:
+      return Bodies.polygon(x, y, sides, Common.random(25, 50), { chamfer: chamfer });
+    }
   });
 
+  World.add(world, stack);
+
+  World.add(world, [
+    // walls
+    Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+    Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+    Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+    Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+  ]);
+
+  // add mouse control
+  var mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false
+        }
+      }
+    });
+
   World.add(world, mouseConstraint);
+
+  // keep the mouse in sync with rendering
+  render.mouse = mouse;
+
+  // fit the render viewport to the scene
+  Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: 800, y: 600 }
+  });
 
   // context for MatterTools.Demo
   return {
@@ -88,4 +103,4 @@ function MatterBasic() {
       Matter.Runner.stop(runner);
     }
   };
-}
+};
