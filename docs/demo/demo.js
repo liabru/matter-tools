b64ac7d90d3f45fd2940f68fd09c00a2f5b712ba
liabru <liabru@brm.io>
2021-01-09 18:45:31 +0000

    update demo

---
+++ docs/demo/demo.js
@@ -0,0 +1,40 @@
+var sourceLinkRoot = 'https://github.com/liabru/matter-tools/blob/master';
+
+MatterTools.Demo.create({
+  inline: false,
+  preventZoom: true,
+  resetOnOrientation: true,
+  startExample: true,
+  appendTo: document.body,
+
+  toolbar: {
+    title: 'matter-tools',
+    url: 'https://github.com/liabru/matter-tools',
+    reset: true,
+    source: true,
+    inspector: true,
+    tools: true,
+    fullscreen: true,
+    exampleSelect: true
+  },
+
+  tools: {
+    inspector: true,
+    gui: true
+  },
+  
+  examples: [
+    {
+      name: 'Basic',
+      id: 'basic',
+      init: Example.basic,
+      sourceLink: sourceLinkRoot + '/docs/examples/basic.js'
+    },
+    {
+      name: 'Basic 2',
+      id: 'basic-2',
+      init: Example.basic,
+      sourceLink: sourceLinkRoot + '/docs/examples/basic.js'
+    }
+  ]
+});
