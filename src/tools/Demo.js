31c67f9a44c88deb02bfd79a7b62c6223657d1d6
liabru <liabru@brm.io>
2021-01-11 23:05:19 +0000

    improve demo toolbar scaling, spacing and alignment

--- src/tools/Demo.js
+++ src/tools/Demo.js
@@ -422,7 +422,7 @@ Demo._createDom = function(options) {
               </svg>
               </button>
               <button class="matter-btn matter-btn-fullscreen" title="Fullscreen">
-                <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <svg fill="#000000" height="22" viewBox="0 0 22 22" width="22" xmlns="http://www.w3.org/2000/svg">
                   <path d="M0 0h24v24H0z" fill="none"/>
                   <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
                 </svg>
