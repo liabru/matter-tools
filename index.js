2f6e0dc0b664ea44179519c7e3ad32886ce3dc7b
liabru <liabru@brm.io>
2021-02-03 00:00:50 +0000

    change package main to build

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  Gui: require('./src/tools/Gui'),
-  Inspector: require('./src/tools/Inspector'),
-  Demo: require('./src/tools/Demo'),
-  Serializer: require('./src/tools/Serializer')
-};
